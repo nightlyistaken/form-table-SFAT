aa465ccc351461e6ecc879fd23931b417222ad9d
dhairy-online <dhairysrivastava5@gmail.com>
2021-07-05 22:32:06 +0530

    0.1
    	modified:   main.css

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
+// Made by breadA#3012
+// for the whole form
 function insert() {
   let n = document.getElementById("name").value;
   let c = document.getElementById("class").value;
+  let data = setData(n, c);
 
-  let prev_table = document.getElementById("result").innerHTML;
-  let new_table = prev_table + "<tr><td>" + n + "</td><td>" + c + "</td></tr>";
-  document.getElementById("result").innerHTML = new_table;
+  let htmlTable = document.getElementById("result");
+  let new_table = "<thead>  <tr>  <td>Name</td> <td>Class</td></tr></thead>";
+  data.forEach((e) => {
+    new_table = new_table + "<tr><td>" + e.firstName + "</td><td>" +
+      e.className + "</td></tr>";
+  });
+
+  htmlTable.innerHTML = new_table;
+
+  // const targetDiv = document.getElementById("div");
+  // const btn = document.getElementById("toggle");
+
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+  } else {
+    targetDiv.style.display = "block";
+  }
+}
+
+function alertmsg() {
+  alert("Please reload the page to apply changes.");
+}
+// to understand lol
+function setData(n, c) {
+  let data = localStorage.getItem("data");
+  if (!data) {
+    data = [];
+  } else {
+    data = JSON.parse(data);
+  }
+  console.log("data", data);
+  if (n && c) {
+    data.push({ firstName: n, className: c });
+  }
+  localStorage.setItem("data", JSON.stringify(data));
+  return data;
 }
