5b04a648a311235499ec42034f50be3d28ade538
dhairy-online <dhairysrivastava5@gmail.com>
2021-07-06 14:42:40 +0530

    0.2

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Made by breadA#3012
 // for the whole form
+
+function deleteData(firstName) {
+  let data = localStorage.getItem("data");
+  data = JSON.parse(data);
+  const updatedData = data.filter(v => v.firstName !== firstName);
+  localStorage.setItem("data", JSON.stringify(updatedData));
+  location.reload();
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 function insert() {
   let n = document.getElementById("name").value;
   let c = document.getElementById("class").value;
@@ -8,14 +17,21 @@ function insert() {
   let htmlTable = document.getElementById("result");
   let new_table = "<thead>  <tr>  <td>Name</td> <td>Class</td></tr></thead>";
   data.forEach((e) => {
-    new_table = new_table + "<tr><td>" + e.firstName + "</td><td>" +
-      e.className + "</td></tr>";
+    new_table =
+      new_table +
+      "<tr><td>" +
+      e.firstName +
+      "</td><td>" +
+      e.className +
+      "</td><td>" +
+      "<button onclick='deleteData(\""+ e.firstName + "\")'> Delete </button>" +
+      "</td></tr>";
   });
 
   htmlTable.innerHTML = new_table;
 
-  // const targetDiv = document.getElementById("div");
-  // const btn = document.getElementById("toggle");
+  const targetDiv = document.getElementById("div");
+  const btn = document.getElementById("toggle");
 
   if (targetDiv.style.display !== "none") {
     targetDiv.style.display = "none";
@@ -23,18 +39,21 @@ function insert() {
     targetDiv.style.display = "block";
   }
 }
-
+// 
 function alertmsg() {
-  alert("Please reload the page to apply changes.");
+  location.reload();
 }
-// to understand lol
-function setData(n, c) {
+
+// This function sets data into localStorage and re-renders the table
+function setData(n, c) { 
   let data = localStorage.getItem("data");
+  
   if (!data) {
     data = [];
   } else {
     data = JSON.parse(data);
   }
+
   console.log("data", data);
   if (n && c) {
     data.push({ firstName: n, className: c });
@@ -42,4 +61,8 @@ function setData(n, c) {
   localStorage.setItem("data", JSON.stringify(data));
   return data;
 }
-//ok
+
+function clearfield() {
+  document.getElementById("name").value = "";
+  document.getElementById("class").value = "";
+}
