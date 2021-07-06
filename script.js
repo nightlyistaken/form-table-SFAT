c8f92c10898db09161f1e9913f4eb39c8a8bcd16
dhairy-online <dhairysrivastava5@gmail.com>
2021-07-06 16:47:36 +0530

    Mmajor change: v1.0

--- script.js
+++ script.js
@@ -4,28 +4,31 @@
 function deleteData(firstName) {
   let data = localStorage.getItem("data");
   data = JSON.parse(data);
-  const updatedData = data.filter(v => v.firstName !== firstName);
+  const updatedData = data.filter((v) => v.firstName !== firstName);
   localStorage.setItem("data", JSON.stringify(updatedData));
   location.reload();
 }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+
 function insert() {
   let n = document.getElementById("name").value;
   let c = document.getElementById("class").value;
   let data = setData(n, c);
 
   let htmlTable = document.getElementById("result");
-  let new_table = "<thead>  <tr>  <td>Name</td> <td>Class</td></tr></thead>";
+  let new_table =
+    "<thead>  <tr>  <td>Name</td> <td>Class</td> <td>Action</td></tr></thead>";
   data.forEach((e) => {
     new_table =
       new_table +
-      "<tr><td>" +
+      "<tbody id='myTable'> <tr><td>" +
       e.firstName +
       "</td><td>" +
       e.className +
       "</td><td>" +
-      "<button onclick='deleteData(\""+ e.firstName + "\")'> Delete </button>" +
-      "</td></tr>";
+      "<button class= 'btn btn-outline-danger btn-sm ' onclick='deleteData(\"" +
+      e.firstName +
+      "\")'> Delete </button>" +
+      "</td></tr></tbody>";
   });
 
   htmlTable.innerHTML = new_table;
@@ -39,15 +42,15 @@ function insert() {
     targetDiv.style.display = "block";
   }
 }
-// 
+//
 function alertmsg() {
   location.reload();
 }
 
 // This function sets data into localStorage and re-renders the table
-function setData(n, c) { 
+function setData(n, c) {
   let data = localStorage.getItem("data");
-  
+
   if (!data) {
     data = [];
   } else {
@@ -66,3 +69,23 @@ function clearfield() {
   document.getElementById("name").value = "";
   document.getElementById("class").value = "";
 }
+
+function save(){
+
+  html2canvas(document.getElementById("result"), {
+    onrendered: function(canvas) {
+        var img = canvas.toDataURL("image/png");
+        const imageDiv = document.getElementById("img-display");
+        imageDiv.innerHTML = '<a download="SFAT Table Save.png" href="'+img+'">test</a>';
+    }
+  });
+
+}
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
